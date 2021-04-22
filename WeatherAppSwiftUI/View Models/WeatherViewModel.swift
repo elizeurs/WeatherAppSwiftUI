b6845e95c79b5ec46eafe8beccd62988235052a0
elizeurs <13ders@gmail.com>
2021-04-22 17:46:18 -0300

    Implementing AddWeatherViewModel

---
+++ WeatherAppSwiftUI/View Models/WeatherViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherViewModel.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Elizeu RS on 22/04/21.
+//
+
+import Foundation
+
+struct WeatherViewModel {
+  
+  let weather: Weather
+  
+  var temperature: Double {
+    return weather.temperature
+  }
+  
+  var city: String {
+    return weather.city
+  }
+  
+  var icon: String {
+    return weather.icon
+  }
+  
+  var sunrise: Date {
+    return weather.sunrise
+  }
+  
+  var sunset: Date {
+    return weather.sunset
+  }
+}
