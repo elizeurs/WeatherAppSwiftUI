40bb59d25f4cc8b39d5a9ed994c1d894ba2e76d6
elizeurs <13ders@gmail.com>
2021-04-22 15:32:54 -0300

    Implementing the Models

--- WeatherAppSwiftUI/Utils/Constants.swift
+++ WeatherAppSwiftUI/Utils/Constants.swift
@@ -10,7 +10,8 @@ import Foundation
 struct Constants {
     struct Urls {
         static func weatherByCity(city: String) -> URL? {
-            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ef0fd9866ca027e0dca474cee84c53be")
+//            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ef0fd9866ca027e0dca474cee84c53be")
+          return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=277520a0e94adef880012bc37e66ef12")
         }
         
         static func weatherUrlAsStringByIcon(icon: String) -> String {
